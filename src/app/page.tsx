07eb0391b93e186ce4f97daf89e771840fb32cec
Andrew Labutin <Labutin.99@bk.ru>
2025-09-28 21:39:37 +0700

    added suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,12 @@
-import SearchInput from "../components/SearchInput/SearchInput";
+import { Suspense } from "react";
+import SearchInput from "@/components/SearchInput/SearchInput";
+
 export default function Home() {
-	return <SearchInput />;
+	return (
+		<div>
+			<Suspense fallback={<div>Загрузка...</div>}>
+				<SearchInput />
+			</Suspense>
+		</div>
+	);
 }
